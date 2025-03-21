e5922cf1f18961520f757564713cdf88daa41ec9
NicolasRuizGiolitoUni <nruiz@uni-bremen.de>
2025-03-21 13:53:26 +0100

    added input option to accordion

--- src/components/list-options/ListOptions.jsx
+++ src/components/list-options/ListOptions.jsx
@@ -1,36 +1,54 @@
-import { useState } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Collapse from 'react-bootstrap/Collapse';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import Collapse from "react-bootstrap/Collapse";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ListGroupItem } from "react-bootstrap";
+import "./ListOptions.css";
+import Form from "react-bootstrap/Form";
 
-import './ListOptions.css'
-
-function OptionsList() {
+function ListOptions() {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [innerSelectedOption, setInnerSelectedOption] = useState(null);
   const [openAccordion, setOpenAccordion] = useState(null);
 
   const options = [
-    { type: 'list', label: "It was more popular" },
-    { type: 'accordion', label: "I wanted to communicate with someone particular", body: "Who did you wanted to communicate with?" },
-    { type: 'list', label: "I liked a feature" },
-    { type: 'list', label: "I didn't like the content on Facebook" },
+    { type: "list", label: "It was more popular" },
+    {
+      type: "accordion",
+      label: "I wanted to communicate with someone particular",
+      body: {
+        title: "Who did you want to communicate with?",
+        options: [
+          { type: "text", label: "Friends" },
+          { type: "text", label: "Family" },
+          { type: "text", label: "Colleagues" },
+          { type: "input", label: "Other" },
+        ],
+      },
+    },
+    { type: "list", label: "I liked a feature" },
+    { type: "list", label: "I didn't like the content on Facebook" },
   ];
 
   return (
     <ListGroup>
       {options.map((option, index) => {
-        if (option.type === 'list') {
+        if (option.type === "list") {
           return (
             <ListGroup.Item
               action
               key={index}
               active={selectedOption === option.label}
-              onClick={() => setSelectedOption(option.label)}
+              onClick={() => {
+                setSelectedOption(option.label);
+                setOpenAccordion(null);
+                setInnerSelectedOption(null);
+              }}
             >
               {option.label}
             </ListGroup.Item>
           );
-        } else if (option.type === 'accordion') {
+        } else if (option.type === "accordion") {
           const isOpen = openAccordion === option.label;
           return (
             <div key={index}>
@@ -47,8 +65,31 @@ function OptionsList() {
               <Collapse in={isOpen}>
                 <div id={`accordion-body-${index}`}>
                   {/* Extra wrapper with no margin or padding */}
-                  <div className='accordion-content'>
-                    {option.body}
+                  <div className="accordion-content">
+                    <p className="accordion-title">{option.body.title}</p>
+                    <ListGroup>
+                      {option.body.options.map((innerOption, innerIndex) => (
+                        <ListGroupItem
+                          action
+                          key={innerIndex}
+                          active={innerSelectedOption === innerOption.label}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setInnerSelectedOption(innerOption.label);
+                          }}
+                        >
+                          {innerOption.type == "text" ? (
+                            innerOption.label
+                          ) : (
+                            <Form.Control
+                              type="text"
+                              id="iput"
+                              placeholder="Other"
+                            />
+                          )}
+                        </ListGroupItem>
+                      ))}
+                    </ListGroup>
                   </div>
                 </div>
               </Collapse>
@@ -61,4 +102,4 @@ function OptionsList() {
   );
 }
 
-export default OptionsList;
+export default ListOptions;
